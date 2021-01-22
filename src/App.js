52ded17dccc87e6c2d339f89c09e0cdf2334c4c9
rvalle <rvalle@antel.net.uy>
2021-01-22 15:26:17 -0300

    redirect to home if the user SignIn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
 
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
@@ -57,16 +57,21 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />):(<SignInAndSignUpPage />)}/>
         </Switch>
 
       </div>
     );
   }
 }
+//redireccionar para evitar entrar al sign mientras esta logueado
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
 
+ //conectar el user-reducer
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,3 +1,5 @@
+import {userActionTypes} from './user.types';
+
 const INITIAL_STATE = {
     currentUser:null
 }
@@ -7,7 +9,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
     
     switch(action.type)
       {
-        case 'SET_CURRENT_USER':
+        case userActionTypes.SET_CURRENT_USER:
         return {
             ...state,
             currentUser: action.payload
